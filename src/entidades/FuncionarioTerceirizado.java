0932770822afa3d478f37894b4794c131ac60291
João da Mata <jlfilho@uea.edu.br>
2022-08-26 12:00:08 -0400

    Primeiro commit

---
+++ src/entidades/FuncionarioTerceirizado.java
@@ -0,0 +1,28 @@
+package entidades;
+
+public class FuncionarioTerceirizado extends Funcionario {
+
+	private Double additionalCharge;
+
+	public FuncionarioTerceirizado() {
+		super();
+	}
+	
+	public FuncionarioTerceirizado(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+		super(name, hours, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
+
+	public Double getAdditionalCharge() {
+		return additionalCharge;
+	}
+
+	public void setAdditionalCharge(Double additionalCharge) {
+		this.additionalCharge = additionalCharge;
+	}
+	
+	@Override
+	public double payment() {
+		return super.payment() + additionalCharge * 1.1;
+	}
+}
